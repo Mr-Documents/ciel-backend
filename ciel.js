9b9869905375c9e52b8212c71c74844a488de05a
samir ibrahim <114286708+samir419@users.noreply.github.com>
2025-05-25 09:17:53 +0100

    new features

--- ciel.js
+++ ciel.js
@@ -67,17 +67,31 @@ const chatSchema = new mongoose.Schema({
 })
 
 const forumSchema = new mongoose.Schema({
+  name: String,
+  description:String,
   owner:{ type: Schema.Types.ObjectId, ref: 'User' },
   members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   topics:[String],
   createdAt: { type: Date, default: Date.now }
 });
 
-const forumpostSchema = new mongoose.Schema({
-  author:{ type: Schema.Types.ObjectId, ref: 'User' },
- forum:{ type: Schema.Types.ObjectId, ref: 'Forum' },
- topic:String,
- comments:[{author:{ type: Schema.Types.ObjectId, ref: 'User' },content:String}],
+const forumtopicschema = new mongoose.Schema({
+  name: String,
+  description:String,
+  forum:{ type: Schema.Types.ObjectId, ref: 'Forum' },
+  posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+const productschema = new mongoose.Schema({
+  name: String,
+  description: String,
+  picture: String,
+  price:Number,
+  comments:[{author:{ type: Schema.Types.ObjectId, ref: 'User' },content:String,likes:[{ type: Schema.Types.ObjectId, ref: 'User' }]}],
+  seller: { type: Schema.Types.ObjectId, ref: 'User' },
+  tags:[String],
+  quantity:Number,
   createdAt: { type: Date, default: Date.now }
 })
 
@@ -86,7 +100,8 @@ const Post = mongoose.model('Post',postSchema);
 const Collection = mongoose.model('Collection',collection);
 const Chat = mongoose.model('Chat',chatSchema);
 const Forum = mongoose.model('Forum',forumSchema);
-const ForumPost = mongoose.model('ForumPost',forumpostSchema);
+const ForumTopic = mongoose.model('ForumTopic',forumtopicschema);
+const Product = mongoose.model('Product',productschema)
 
 app.get('/', async (req, res) => {
   res.json({message:"hello backend"})
@@ -142,6 +157,11 @@ app.post('/create_post/:id',upload.single('file'),async (req,res)=>{
   res.json(newPost)
 })
 
+app.get('/post/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id).populate('author').populate({path:'comments', populate: {path: 'author'}}).exec();
+  res.json(post)
+});
+
 app.get('/user/:id', async (req,res)=>{
   const user = await User.findById(req.params.id);
   res.json(user)
@@ -170,43 +190,6 @@ app.post('/followuser/:id',async (req,res)=>{
   const following = await User.findByIdAndUpdate(req.body.user,{ $push: { following: { $each: [following_id] } } })
   res.json({message:`followed ${follow} and ${following}`})
 })
-
-app.post('/createforum/:id',async(req,res)=>{
-  const forum = new Forum({
-    owner: req.params.id,
-    members:[req.params.id]
-  })
-  await forum.save()
-  res.json(forum)
-})
-
-app.post('/createforumtopic/:id',async(req,res)=>{
- const forum = await Forum.findByIdAndUpdate(req.params.id,{ $push: { topics: { $each: [{name:req.body.topic}] } } })
-  res.json(forum)
-})
-
-app.post('/createforumpost/:id',async(req,res)=>{
-  const post = new ForumPost({
-    author:req.body.user,
-    content:req.file.originalname,
-    title:req.body.title,
-    body:req.body.text,
-    forum:req.body.forum,
-    topic:req.body.topic
-    })
-  const forum = await Forum.findByIdAndUpdate(req.params.id,{ $push: { topics: { $each: [{name:req.body.topic}] } } })
-   res.json(forum)
- })
-
-app.post('/commentforumpost/:id',async(req,res)=>{
-const forum = await Forum.findByIdAndUpdate(req.params.id,{ $push: { topics: { $each: [{name:req.body.topic}] } } })
-  res.json(forum)
-})
-
-app.get('forumlist',async(req,res)=>{
-  const forums = await Forum.find()
-})
-
 
 app.post('/newchat', async (req,res)=>{
   const chat_room = new Chat({
@@ -240,39 +223,97 @@ app.post('/join_chat/:id',async(req,res)=>{
   res.json(chat)
 })
 
-
-
-
-
-
-
-
-
-app.get('/mycollections/:id', async (req,res)=>{
-  const collections = await Collection.find({owner:req.params.id}).populate('posts');
-  res.json(collections)
-})
-app.get('/myposts/:id', async (req,res)=>{
-  const posts = await Post.find({author:req.params.id});
-  res.json(posts)
+app.post('/createforum',async(req,res)=>{
+  const forum = new Forum({
+    name: req.body.forum_name,
+    description: req.body.description,
+    owner: req.body.user,
+    members:[req.body.user]
+  })
+  await forum.save()
+  res.json(forum)
 })
 
-app.post('/newcollection',async (req,res)=>{
-  const collection = new Collection({
+app.get('/myforums/:id',async(req,res)=>{
+  const forums = await Forum.find({owner:req.params.id})
+  res.json(forums)
+})
+
+app.get('/forumlist',async(req,res)=>{
+  const forums = await Forum.find()
+  res.json(forums)
+})
+
+app.get('/forum/:id',async(req,res)=>{
+  const forum = await Forum.findById(req.params.id).populate('topics')
+  const topics = await ForumTopic.find({forum:req.params.id}).populate('posts')
+  res.json(topics)
+})
+
+app.post('/createtopic',async(req,res)=>{
+  const topic = new ForumTopic({
+    name: req.body.topic_name,
+    description: req.body.description,
+    forum: req.body.forum,
+  })
+  await topic.save()
+  res.json(topic)
+})
+
+app.get('/topic/:id',async(req,res)=>{
+  const topic = await ForumTopic.findById(req.params.id).populate('posts')
+  res.json(topic)
+})
+
+app.post('/createforumpost',upload.single('file'),async (req,res)=>{
+  console.log(req.body)
+  const newPost = new Post({
+    title: req.body.title,
+    content: req.file?.originalname ?? 'none',
+    description: req.body.description,
+    type: req.body.type,
+    author: req.body.user,
+  });
+  await newPost.save();
+  const topic = await ForumTopic.findByIdAndUpdate(req.body.topic,{ $push: { posts: { $each: [newPost._id] } } })
+  res.json(newPost,topic)
+})
+
+app.get('/products', async (req, res) => {
+  const products = await Product.find().sort({ createdAt: -1 }).populate('seller').populate({path:'comments', populate: {path: 'author'}}).exec();
+  res.json(products)
+});
+
+app.get('/products/:id', async (req, res) => {
+  const id = req.params.id;
+  const products = await Product.find({seller:id});
+  res.json(products)
+});
+
+app.get('/product/:id', async (req, res) => {
+  const product = await Product.findOne({_id:req.params.id});
+  res.json(product)
+});
+
+app.post('/create_product/:id',upload.single('file'),async (req,res)=>{
+  const product = new Product({
     name: req.body.name,
-    owner: req.body.owner,
-    ispublic: false
+    picture: req.file?.originalname ?? 'none',
+    description: req.body.description,
+    seller: req.params.id,
+    price: req.body.price,
+    quantity: req.body.quantity
   });
 
-  await collection.save();
-  console.log('collection Created:', collection);
+  await product.save();
+  res.json(product)
 })
 
-app.post('/addtocollection',async (req,res)=>{
-  const collection = await Collection.updateOne({ _id: req.body.collection_id }, { $push: { posts: { $each: [req.body.id] } } });
-  console.log('post added:', collection);
+app.post('/comment_product/:id',async (req,res)=>{
+  const comment = {author:req.body.user,content:req.body.message}
+  const product = await Product.findByIdAndUpdate(req.params.id,{ $push: { comments: { $each: [comment] } } })
+  res.json(product)
 })
-
 
 
 server.listen(5000, () => {
